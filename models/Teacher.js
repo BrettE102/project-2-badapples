75405840fda67bc1343c897cfe5a95d54edd2665
twangerwang <108110493+twangerwang@users.noreply.github.com>
2022-11-07 14:41:11 -0600

    added user.js

--- models/Teacher.js
+++ models/Teacher.js
@@ -1,25 +1,31 @@
-// requiring sequelize 
-const { Model, DataTypes } = require('sequelize');
+// requiring sequelize
+const { Model, DataTypes } = require("sequelize");
 
 class Teacher extends Model {}
 
 // Setting parameter rules for teachers
 Teacher.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            autoIncrement: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        subject: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
     },
-)
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subject: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    underscored: true,
+    modelName: "teacher",
+  }
+);
 
-module.exports = Teacher
+module.exports = Teacher;
