2f637889bc5f33050349b18928f343e50b9df510
twangerwang <108110493+twangerwang@users.noreply.github.com>
2022-11-10 18:50:58 -0600

    added files in Public folder

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,13 @@
-const Sequelize = require('sequelize')
-let sequelize;
+const Sequelize = require("sequelize");
 
-module.exports = sequelize
+require("dotenv").config();
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: process.env.DB_HOST,
+      dialect: "mysql",
+      port: 3001,
+    });
+
+module.exports = sequelize;
--- controllers/api/teacherRoutes.js
+++ controllers/api/teacherRoutes.js
@@ -1,3 +1,3 @@
 const router = require("express").Router();
-const { Project } = require("../../models");
+const { Course, StudentCourses, Student } = require("../../models");
 const withAuth = require("../../utils/auth");
