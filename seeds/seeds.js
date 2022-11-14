1875ea203d7664126963d2e68d89207fd121e14e
twangerwang <108110493+twangerwang@users.noreply.github.com>
2022-11-13 21:34:46 -0600

    fixed table drop issue

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,14 +1,16 @@
 const sequelize = require("../config/connection");
-const { Student, Teacher } = require("../models");
+const { Student, Teacher, User } = require("../models");
 
 const teacherSeedData = require("./teacherData.json");
 const studentSeedData = require("./studentData.json");
 
 const seedDatabase = async () => {
+  await Student.drop();
+  console.log("student table dropped!");
   await sequelize.sync({ force: true });
 
-  const teacher = await Teacher.bulkCreate(teacherSeedData);
-  const student = await Student.bulkCreate(studentSeedData);
+  // const teacher = await Teacher.bulkCreate(teacherSeedData);
+  // const student = await Student.bulkCreate(studentSeedData);
 
   process.exit(0);
 };
